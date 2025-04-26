1d3cbd09fe062656e3ce124a470b2986292e7be1
lichengguang <lcgsmile.qq.com>
2025-04-27 01:54:09 +0800

    Remove build, log and install directories from tracking

--- build/interactive_marker_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lcg/ros2_learning_examples/interactive_marker_demo
-# Build directory: /home/lcg/ros2_learning_examples/build/interactive_marker_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/interactive_marker_tutorials/CMakeFiles/interactive_marker_tutorials_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for interactive_marker_tutorials_uninstall.
